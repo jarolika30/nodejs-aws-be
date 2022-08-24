b8db51d89bf0d30548a9f8a0ed575c132a3a2c46
Katsiaryna Staurova <Katsiaryna_Staurova@epam.com>
2022-08-24 20:56:03 +0300

    create getProduct lambda

--- product-service/handler.js
+++ product-service/handler.js
@@ -1,16 +1,12 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+module.exports.getProduct = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: {
+      productName: "Stone",
+      price: 123
+    }
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
